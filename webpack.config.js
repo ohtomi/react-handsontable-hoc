db035926aa33ab7f672fb06f86fb31fc0ebb059b
ohtomi <ohtomi.kenichi@gmail.com>
2017-08-13 11:54:31 +0900

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname + '/dist'),
+        filename: 'index.js',
+        library: 'ReactHansontableHoc',
+        libraryTarget: 'umd'
+    },
+    module: {
+        loaders: [{
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader'
+        }]
+    },
+    externals: {
+        'react': 'react',
+        'react-dom': 'react-dom'
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname + '/dist'),
+        publicPath: '/',
+        watchContentBase: true
+    }
+};
