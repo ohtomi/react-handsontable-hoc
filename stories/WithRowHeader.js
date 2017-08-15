55a0e20d90b58e73be71b3905fe76bac8c263446
ohtomi <ohtomi.kenichi@gmail.com>
2017-08-16 00:08:09 +0900

    Remove trailing comma

--- stories/WithRowHeader.js
+++ stories/WithRowHeader.js
@@ -9,7 +9,7 @@ const data = [
     {'id': 1, 'name': 'ford', 'year': 2018, 'volume': 1000, 'good': true},
     {'id': 2, 'name': 'volvo', 'year': 2017, 'volume': 1000, 'good': false},
     {'id': 3, 'name': 'toyota', 'year': 2016, 'volume': 1000, 'good': true},
-    {'id': 4, 'name': 'honda', 'year': 2015, 'volume': 1000, 'good': true},
+    {'id': 4, 'name': 'honda', 'year': 2015, 'volume': 1000, 'good': true}
 ];
 
 const columns = [
