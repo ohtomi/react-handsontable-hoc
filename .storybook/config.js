178c145823ff3cdf7626e169664b14ab1c385d29
ohtomi <ohtomi.kenichi@gmail.com>
2018-11-03 19:04:00 +0900

    Add sample for persistent state plugin

--- .storybook/config.js
+++ .storybook/config.js
@@ -7,6 +7,7 @@ function loadStories() {
     require('../src/stories/RowFilter')
     require('../src/stories/SelectionMode')
     require('../src/stories/Viewport')
+    require('../src/stories/PersistentState')
     require('../src/stories/CustomRenderer')
 }
 
